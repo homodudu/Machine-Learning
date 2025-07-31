c6ee7453aba5f14c49fdeadddc9a46de63921505
lullobytes <48366159+homodudu@users.noreply.github.com>
2025-07-31 21:12:52 +0100

    Code refactoring.

--- NETwork/ai_search_handler.py
+++ NETwork/ai_search_handler.py
@@ -13,6 +13,9 @@ from azure.search.documents.indexes.models import (
 # Load environment variables from .env file
 load_dotenv()
 
+# Azure Open AI settings
+OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
+
 # Azure Blob Storage settings
 BLOB_CONNECTION_STRING = os.getenv("AZURE_BLOB_CONNECTION_STRING")
 BLOB_CONTAINER_NAME = os.getenv("AZURE_BLOB_CONTAINER_NAME")
@@ -27,14 +30,17 @@ SEARCH_INDEXER_NAME =  os.getenv("AZURE_AI_SEARCH_INDEXER")
 SEARCH_INDEXER_PATH = os.getenv("AZURE_AI_SEARCH_INDEXER_CONFIG_PATH")
 SEARCH_SKILLSET_NAME =  os.getenv("AZURE_AI_SEARCH_SKILLSET")
 SEARCH_SKILLSET_PATH = os.getenv("AZURE_AI_SEARCH_SKILLSET_CONFIG_PATH")
+SEARCH_VECTORIZER_NAME = os.getenv("AZURE_AI_SEARCH_VECTORIZER")
 
 # Initialize Azure Search Indexer Client
 index_client = SearchIndexClient(endpoint=SEARCH_ENDPOINT, credential=AzureKeyCredential(SEARCH_API_KEY))
+# index_client.delete_index(SEARCH_INDEX_NAME)
 
 # Initialize Azure Search Indexer Client
 indexer_client = SearchIndexerClient(endpoint=SEARCH_ENDPOINT, credential=AzureKeyCredential(SEARCH_API_KEY))
 
-def create_or_update_datasource_connection(data_source_name, container_name, connection_string):
+# Function to create or update the data source connection
+def _create_or_update_datasource_connection(data_source_name, container_name, connection_string):
     # Create the data source connection for Azure Blob Storage
     data_source_connection = SearchIndexerDataSourceConnection(
         name=data_source_name,
@@ -49,8 +55,9 @@ def create_or_update_datasource_connection(data_source_name, container_name, con
     indexer_client.create_or_update_data_source_connection(data_source_connection)
     print(f"Data source '{data_source_name}' connection created or updated.\n")
 
-def deploy_json_config_via_api_call(filepath, url, api_key):
-    headers = {"Content-Type": "application/json", "api-key": api_key }
+# Function to deploy azure search JSON configuration via API call
+def _deploy_json_config_via_api_call(filepath, url, api_key):
+    headers = {"Content-Type": "application/json", "api-key": api_key}
     with open(filepath) as f:
         index_json = f.read()
     try:
@@ -60,35 +67,56 @@ def deploy_json_config_via_api_call(filepath, url, api_key):
     except requests.exceptions.HTTPError as e:
         raise Exception(f"JSON configuration from '{filepath}' has failed deployment to Azure search (status code: {response.status_code})")
 
-def create_or_update_search_index(index_name, filepath,  api_endpoint, api_key):
-    # Deploy the JSON index configuration to Azure search
-    deploy_json_config_via_api_call(filepath,f"{api_endpoint}indexes/{index_name}?api-version=2025-05-01-Preview", api_key)
+# Function to create or update the search index
+def _create_or_update_search_index(index_name, filepath,  api_endpoint, api_key):
+    _deploy_json_config_via_api_call(filepath,f"{api_endpoint}indexes/{index_name}?api-version={OPENAI_API_VERSION}", api_key)
     print(f"Index '{index_name}' created or updated via JSON.\n")
 
-def create_or_update_search_indexer(indexer_name, filepath,  api_endpoint, api_key):
-    # Deploy the JSON indexer configuration to Azure search
-    deploy_json_config_via_api_call(filepath, f"{api_endpoint}indexers/{indexer_name}?api-version=2025-05-01-Preview", api_key)
+# Function to create or update the search indexer
+def _create_or_update_search_indexer(indexer_name, filepath,  api_endpoint, api_key):
+    _deploy_json_config_via_api_call(filepath, f"{api_endpoint}indexers/{indexer_name}?api-version={OPENAI_API_VERSION}", api_key)
     print(f"Indexer '{indexer_name}' created or updated via JSON.\n")
 
-def create_or_update_indexer_skillset(skillset_name, filepath,  api_endpoint, api_key):
-    # Deploy the JSON skillset configuration to Azure search
-    deploy_json_config_via_api_call(filepath,f"{api_endpoint}skillsets/{skillset_name}?api-version=2025-05-01-Preview", api_key)
+# Function to create or update the search indexer skillset
+def _create_or_update_indexer_skillset(skillset_name, filepath,  api_endpoint, api_key):
+    _deploy_json_config_via_api_call(filepath,f"{api_endpoint}skillsets/{skillset_name}?api-version={OPENAI_API_VERSION}", api_key)
     print(f"Skillset '{skillset_name}' skillset created or updated via JSON.\n")
 
-if __name__ == "__main__":
-    # Create or update the data source connection
-    create_or_update_datasource_connection(BLOB_DATA_SOURCE_NAME, BLOB_CONTAINER_NAME, BLOB_CONNECTION_STRING)
+# Main function to create or update Azure Cognitive Search components - to be equipped as agentic tool
+def create_or_update_azure_search(
+        blob_data_source_name,
+        blob_container_name,
+        blob_connection_string,
+        search_index_name,
+        search_index_path,
+        search_indexer_name,
+        search_indexer_path,
+        search_endpoint,
+        search_api_key,
+        search_skillset_name,
+        search_skillset_path
+        ):
 
-    # Create or update the search index
-    create_or_update_search_index(SEARCH_INDEX_NAME, SEARCH_INDEX_PATH, SEARCH_ENDPOINT, SEARCH_API_KEY )
-
-    # Create or update the search indexer
-    create_or_update_search_indexer(SEARCH_INDEXER_NAME, SEARCH_INDEXER_PATH, SEARCH_ENDPOINT, SEARCH_API_KEY )
-
-    # Create or update the search indexer skill set
-    create_or_update_indexer_skillset(SEARCH_SKILLSET_NAME, SEARCH_SKILLSET_PATH, SEARCH_ENDPOINT, SEARCH_API_KEY )
+    _create_or_update_datasource_connection(blob_data_source_name, blob_container_name, blob_connection_string)
+    _create_or_update_search_index(search_index_name, search_index_path, search_endpoint, search_api_key)
+    _create_or_update_search_indexer(search_indexer_name, search_indexer_path, search_endpoint, search_api_key)
+    _create_or_update_indexer_skillset(search_skillset_name, search_skillset_path, search_endpoint, search_api_key)
 
     # Run the indexer to load data from blob into the index
-    # indexer_client.reset_indexer(SEARCH_INDEXER_NAME)
-    indexer_client.run_indexer(SEARCH_INDEXER_NAME)
-    print(f"Indexer '{SEARCH_INDEXER_NAME}' loaded data into index '{SEARCH_INDEX_NAME}'.\n")
+    indexer_client.run_indexer(search_indexer_name)
+    print(f"Indexer '{search_indexer_name}' has ran sucessfully. New data will be indexed into '{search_index_name}' if it exists.\n")
+
+if __name__ == "__main__":
+    create_or_update_azure_search(
+        BLOB_DATA_SOURCE_NAME,
+        BLOB_CONTAINER_NAME,
+        BLOB_CONNECTION_STRING,
+        SEARCH_INDEX_NAME,
+        SEARCH_INDEX_PATH,
+        SEARCH_INDEXER_NAME,
+        SEARCH_INDEXER_PATH,
+        SEARCH_ENDPOINT,
+        SEARCH_API_KEY,
+        SEARCH_SKILLSET_NAME,
+        SEARCH_SKILLSET_PATH
+    )
--- NETwork/github_repo_handler.py
+++ NETwork/github_repo_handler.py
@@ -28,61 +28,74 @@ BLOB_CONTAINER_NAME = os.getenv("AZURE_BLOB_CONTAINER_NAME")
 blob_service_client = BlobServiceClient.from_connection_string(BLOB_CONNECTION_STRING)
 container_client = blob_service_client.get_container_client(BLOB_CONTAINER_NAME)
 
-def _extract_csv_urls_from_github_repo(repo_name):
+# Function to extract and filter CSV URLs from a GitHub repository
+def _extract_and_filter_csv_urls_from_github_repo(repo_name, retrieval_start_year=MOST_RECENT_YEAR):
     repo = github_client.get_repo(repo_name)
     urls = [c.html_url for c in repo.get_contents("") if c.html_url.endswith('.csv')]
-    return urls
-
-def _transform_and_filter_list_of_github_urls(urls, retrieval_start_year):
     urls = [l.replace("github", "raw.githubusercontent").replace("blob/", "refs/heads/") for l in urls]
     urls = [l for l in urls if re.search(r"\d{4}.csv", l)]
     urls = [l for l in urls if int(re.sub(r"\D", "", l)) >= retrieval_start_year]
     return urls
 
+# Function to update the local repository with new Github URLs
 def _update_url_archive_in_local_repo(urls, url_download_path):
     df_urls = pd.DataFrame({"URL": urls})
     initial = not os.path.exists(url_download_path)
+
+    # If the file does not exist, create it and write the URLs
     if initial:
         df_urls.to_csv(url_download_path, sep='\t', encoding='utf_8_sig', index=False)
         return df_urls["URL"], initial
+
+    # If the file exists, read it and append new URLs
     df_url_archive = pd.read_csv(url_download_path, sep=',', index_col=False)
     new_urls = df_urls[~df_urls["URL"].isin(df_url_archive["URL"])]["URL"]
     if not new_urls.empty:
         new_urls.to_csv(url_download_path, sep=',', mode='a', encoding='utf_8_sig', header=False, index=False)
     return new_urls, initial
 
+# Function to download and update new URL content in the local repository
 def _download_and_update_url_content_in_local_repo(new_urls, stat_download_path):
     df_master = pd.DataFrame()
     for url in new_urls:
         df_temp = pd.read_csv(url, sep=',', low_memory=False)
         df_master = pd.concat([df_master, df_temp], ignore_index=True)
+
+    # If the master DataFrame is not empty, append it to the local stats file
     if not df_master.empty:
         df_master.to_csv(stat_download_path, sep=',', encoding='utf_8_sig', mode='a', header=True, index=False)
     return df_master
 
+# Function to download GitHub tennis data to local repository and check for updates
 def _download_github_tennis_data_to_local_repo(repo_name, retrieval_start_year=MOST_RECENT_YEAR):
     tour_type = repo_name[-3:].lower()
     url_local_repo_path = f"{REPO_LOCAL}/{tour_type}_url_data.csv"
     stat_local_repo_path = f"{REPO_LOCAL}/{tour_type}_stat_data.csv"
 
-    urls = _extract_csv_urls_from_github_repo(repo_name)
-    filtered_urls = _transform_and_filter_list_of_github_urls(urls, retrieval_start_year)
+    filtered_urls = _extract_and_filter_csv_urls_from_github_repo(repo_name, retrieval_start_year)
     new_urls, initial = _update_url_archive_in_local_repo(filtered_urls,  url_local_repo_path)
 
+    # If there are no new URLs and it's not the initial run, do not update the local repository
     if not initial and new_urls.empty:
         print("No new URLs found, local repo will not be updated.")
         local_update_required=False
         return local_update_required
+
+    # Else if it is the initial run, create the local repository with the new URLs
     else:
         if not os.path.exists(stat_local_repo_path):
             pd.DataFrame().to_csv(stat_local_repo_path, sep=',', encoding='utf_8_sig', index=False)
 
+        # If there are new URLs, download and print the content
         df_master = _download_and_update_url_content_in_local_repo(new_urls, stat_local_repo_path)
         print(df_master)
         print(f"Updated local data archive with {repo_name} data.")
-        local_update_required=True
-        return local_update_required
 
+        # Flag that the local repository was updated
+        local_repo__updated=True
+        return local_repo__updated
+
+# Function to upload local repository data to Azure Blob Storage and overwrite existing data
 def _upload_local_repo_tennis_data_to_azure_blob_storage(src_directory, repo_name):
     tour_type = repo_name[-3:].lower()
     for filename in os.listdir(src_directory):
@@ -93,12 +106,12 @@ def _upload_local_repo_tennis_data_to_azure_blob_storage(src_directory, repo_nam
                 blob_client.upload_blob(data, overwrite=True)
             print(f"Uploaded '{filename}' from directory '{src_directory}' to blob storage.\n")
 
-# Full ETL process to be deployed as an agentic tool.
+# Main function to update Azure blob storage with Github tennis stat data - to be equipped as an agentic tool.
 def update_tennis_stats_blob_storage_resource():
     repo_list = {REPO_ATP, REPO_WTA}
     for repo in repo_list:
-        local_update_required = _download_github_tennis_data_to_local_repo(repo)
-        if local_update_required:
+        local_repo_updated = _download_github_tennis_data_to_local_repo(repo)
+        if local_repo_updated:
             _upload_local_repo_tennis_data_to_azure_blob_storage(REPO_LOCAL, repo)
         print(f"Github repo handler script executed successfully for repo: {repo}.\n")
 

80bd112c5ede5b621dc819dd3320e08502f7f47d
lullobytes <48366159+homodudu@users.noreply.github.com>
2025-08-26 01:43:54 +0100

    Update handler_agent_conversation.py
    Implemented user input prompt.

--- NETwork/handlers/handler_agent_conversation.py
+++ NETwork/handlers/handler_agent_conversation.py
@@ -6,57 +6,55 @@ import os
 
 load_dotenv()
 
-TENANT_ID = os.getenv("AZURE_TENANT_ID")
-CLIENT_ID = os.getenv("AZURE_CLIENT_ID")
-CLIENT_SECRET = os.getenv("AZURE_CLIENT_SECRET")
-PROJECT_ENDPOINT = os.getenv("AZURE_AI_FOUNDRY_ENDPOINT")
-CONNECT_AGENT_ID = os.getenv("AZURE_AI_FOUNDRY_CONNECT_AGENT_ID")
-
+# Initialise credentials client
 credential = ClientSecretCredential(
-    tenant_id=TENANT_ID,
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET
+    tenant_id=os.getenv("AZURE_TENANT_ID"),
+    client_id=os.getenv("AZURE_CLIENT_ID"),
+    client_secret=os.getenv("AZURE_CLIENT_SECRET")
 )
 
+# Initialise AI foundry project client
 project = AIProjectClient(
     credential=credential,
-    endpoint=PROJECT_ENDPOINT
-)
+        endpoint=os.getenv("AZURE_AI_FOUNDRY_ENDPOINT")
+    )
 
+def main():
+    # Get the connect agent
+    connect_agent = project.agents.get_agent(os.getenv("AZURE_AI_FOUNDRY_CONNECT_AGENT_ID"))
 
-connect_agent = project.agents.get_agent(CONNECT_AGENT_ID)
+    # Create project thread
+    thread = project.agents.threads.create()
+    print(f"Created thread, ID: {thread.id}")
 
+    # Start agent conversation with user prompt input
+    try:
+        while True:
+            content = input("Enter your prompt (type 'quit' to exit):\n")
+            print() # Create a new line
 
-thread = project.agents.threads.create()
-print(f"Created thread, ID: {thread.id}")
+            if content.strip().lower() == "quit":
+                print("Exiting conversation.")
+                break
 
-message = project.agents.messages.create(
-    thread_id=thread.id,
-    role="user",
-    content=
-    """
-    There are 11/8 odds of Keys, Rybakina and Svitolina all to win their 1st round matches at the US open in straight sets.
+            project.agents.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=content
+            )
+            run = project.agents.runs.create_and_process(
+                thread_id=thread.id,
+                agent_id=connect_agent.id
+            )
+            if run.status == "failed":
+                print(f"Run failed: {run.last_error}")
+            else:
+                messages = project.agents.messages.list(thread_id=thread.id, order=ListSortOrder.ASCENDING)
+                for message in messages:
+                    if message.text_messages:
+                        print(f"{message.role}:\n{message.text_messages[-1].text.value}\n")
+    except Exception as e:
+        print(e)
 
-    Calculate and share your own betting odds. Is this a good bet?
-
-    In a table, summarise each player's previous 5 matches across all tournaments. Make an assessment of their strengths and weeakness.
-
-    What are the risks from a betting perspective if one of them plays a left handed player?
-
-    Predict the outcome of their 1st round matches.
-
-    Provide a betting strategy.
-    """
-)
-
-run = project.agents.runs.create_and_process(
-    thread_id=thread.id,
-    agent_id=connect_agent.id)
-
-if run.status == "failed":
-    print(f"Run failed: {run.last_error}")
-else:
-    messages = project.agents.messages.list(thread_id=thread.id, order=ListSortOrder.ASCENDING)
-    for message in messages:
-        if message.text_messages:
-            print(f"{message.role}:\n{message.text_messages[-1].text.value}\n")
+if __name__ == "__main__":
+    main()
